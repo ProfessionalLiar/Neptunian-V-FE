cc07c2d3d7e808e76550de365f27993fbd9d98bd
ProfessionalLiar <66849335+ProfessionalLiar@users.noreply.github.com>
2020-06-12 14:23:48 -0700

    Add files via upload

---
+++ Neptunian Pt3.lua
@@ -0,0 +1,39 @@
+function invis()
+    for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == "Tors" then
+            v.Transparency = 1
+        end
+    end
+    
+    for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == "ra" then
+            v.Transparency = 1
+        end
+    end
+    
+    for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == "rh" then
+            v.Transparency = 1
+        end
+    end
+    
+    for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == "rl" then
+            v.Transparency = 1
+        end
+    end
+    
+    for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == "la" then
+            v.Transparency = 1
+        end
+    end
+    
+    for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == "lh" then
+            v.Transparency = 1
+        end
+    end
+end
+
+invis()
